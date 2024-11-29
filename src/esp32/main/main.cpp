27fd75c5e7e2997467af497441402b5de23251b8
Matheus Cirillo <matheuscirillo2003@gmail.com>
2024-11-28 23:00:47 -0300

    Update servo pulse width settings, refine PID controller parameters add reset on zero error

--- src/esp32/main/main.cpp
+++ src/esp32/main/main.cpp
@@ -18,18 +18,21 @@
 
 extern "C" void app_main(void){
     Servo servo0(13, LEDC_CHANNEL_0);
+    servo0.set_min_pulse_width(1300);
+    servo0.set_max_pulse_width(1800);
     Servo servo1(12, LEDC_CHANNEL_1);
+    servo1.set_min_pulse_width(1300);
+    servo1.set_max_pulse_width(1800);
     Servo servos[] = {servo0, servo1};
 
     AutoAimState auto_aim_state = MANUAL;
 
-    gpio_t handshake(5);
     I2CSlave i2c_slave(21, 22, 0x0A, 0);
 
-    double kp = 0.320;
-    double ki = 0.015;
-    double kd = 0.051;
-    double ks = 0.2;
+    double kp = 0.140;
+    double ki = 0.005;
+    double kd = 0.062;
+    double ks = 0.200;
     PIDController x_controller(kp, ki, kd, ks);
     PIDController y_controller(kp, ki, kd, ks);
     
@@ -48,14 +51,14 @@ extern "C" void app_main(void){
             if (numbers == NULL) continue;
             bool valid_servo_id = numbers->param1 >= 0 && numbers->param1 < sizeof(servos) / sizeof(Servo);
 
-            handshake.set_level(LOW);
-
             switch (numbers->command){
                 case SET_ANGLE:
                     if (!valid_servo_id) break;
                     if (auto_aim_state == ENABLED) break;
                     
                     servos[numbers->param1].set_angle(numbers->param2);
+                    x_controller.reset();
+                    y_controller.reset();
                     break;
 
                 case SET_MIN_PULSE_WIDTH:
@@ -76,7 +79,11 @@ extern "C" void app_main(void){
                 {
                     if (auto_aim_state == MANUAL) break;
                     Coordinate error = {numbers->param1, numbers->param2};
-                    if (error.x == 0 && error.y == 0) break;
+                    if (error.x == 0 && error.y == 0) {
+                        x_controller.reset();
+                        y_controller.reset();
+                        break;
+                    }
 
                     double x_control = x_controller.calculateControl(error.x);
                     double y_control = y_controller.calculateControl(error.y);
@@ -139,8 +146,6 @@ extern "C" void app_main(void){
 
             free(numbers);
         }
-
-        handshake.set_level(HIGH);
 
         free(commands);
         free(received_data);
